23e367af8167261fca8169f61229b8f1296ae3c8
Kanishk <kanishk.411@gmail.com>
2020-11-07 17:30:10 +0530

    first-commit

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,83 @@
+import express from 'express';
+import User from '../models/userModel';
+import { getToken, isAuth } from '../util';
+import bcrypt from 'bcryptjs';
+
+const router = express.Router();
+
+router.put('/:id', isAuth, async (req, res) => {
+  const userId = req.params.id;
+  const user = await User.findById(userId);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = bcrypt.hashSync(req.body.password, 8);
+    }
+    const updatedUser = await user.save();
+    res.send({
+      _id: updatedUser.id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: getToken(updatedUser),
+    });
+  } else {
+    res.status(404).send({ message: 'User Not Found' });
+  }
+});
+
+router.post('/signin', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: getToken(user),
+      });
+      return;
+    }
+  }
+  res.status(401).send({ message: 'Invalid email or password' });
+})
+
+
+router.post('/register', async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8),
+  });
+  const newUser = await user.save();
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser),
+    });
+  } else {
+    res.status(401).send({ message: 'Invalid User Data.' });
+  }
+});
+
+router.get('/createadmin', async (req, res) => {
+  try {
+    const user = new User({
+      name: 'jatin',
+      email: 'patial@bhanu.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    });
+    const newUser = await user.save();
+    res.send(newUser);
+  } catch (error) {
+    res.send({ message: error.message });
+  }
+});
+
+export default router;
